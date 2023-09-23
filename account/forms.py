bcd00954c5b724325adf4595e38c426045efd740
Timon57 <timobasnet@gmail.com>
2023-09-23 22:28:09 +0545

    added password reset functionality

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import UserBase
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm,PasswordResetForm,SetPasswordForm
 
 class RegistrationForm(forms.ModelForm):
     username = forms.CharField(label='Enter Username',min_length=4,max_length=50,help_text='Required')
@@ -71,3 +71,25 @@ class UserEditForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].required = True
         self.fields['email'].required = True
+
+
+class PwdResetForm(PasswordResetForm):
+    email = forms.EmailField(max_length=255,widget=forms.TextInput(attrs={'class':'form-control mb-3','placeholder':'Email','id':'form-email'}))
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        u = UserBase.objects.filter(email=email)
+        if not u:
+            raise forms.ValidationError(
+                'Unfortunately we can not find that email address'
+            )
+        return email
+
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='New password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-newpass'}))
+    new_password2 = forms.CharField(
+        label='Repeat password', widget=forms.PasswordInput(
+            attrs={'class': 'form-control mb-3', 'placeholder': 'New Password', 'id': 'form-new-pass2'}))
+
--- account/urls.py
+++ account/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
 from .import views
-from .forms import UserLoginForm
+from .forms import UserLoginForm,PwdResetForm,PwdResetConfirmForm
 
 
 urlpatterns = [
@@ -10,7 +10,20 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(next_page='/account/login/'),name='logout'),
     path('register/',views.account_registration,name='register'),
     path('activate/<slug:uidb64>/<slug:token>/',views.account_activate, name='activate'),
-    path('profile/edit/',views.edit_details,name='edit-details'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="account/user/password_reset_form.html",
+                                                                 success_url='password_reset_email_confirm',
+                                                                 email_template_name='account/user/password_reset_email.html',
+                                                                 form_class=PwdResetForm), name='pwdreset'),
+
+    
+    path('password_reset_confirm/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(template_name='account/user/password_reset_confirm.html',
+                                                                                               success_url = 'password_reset_complete/',
+                                                                                               form_class=PwdResetConfirmForm),name='password_reset_confirm'),
+    path('password_reset/password_reset_email_confirm/',TemplateView.as_view(template_name="account/user/reset_status.html"),name='password_reset_done'),
+                                                                                                
+    path('password_reset_confirm/MTE/password_reset_complete/',
+         TemplateView.as_view(template_name="account/user/reset_status.html"), name='password_reset_complete'),
+    path('profile/edit/', views.edit_details, name='edit-details'),
     path('dashboard/',views.dashboard,name='dashboard'),
     path('profile/delete_user',views.delete_user,name='delete-user'),
     path('profile/delete_confirm/',TemplateView.as_view(template_name="account/user/delete_confirm.html"),name='delete-confirmation')
